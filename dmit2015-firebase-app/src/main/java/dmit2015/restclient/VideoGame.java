bdded4f4789dfb65710ffef9da50ed10a0567221
Sam Wu <swu@nait.ca>
2024-01-23 15:00:27 -0700

    Configure JAR for cloud environment.

--- dmit2015-firebase-app/src/main/java/dmit2015/restclient/VideoGame.java
+++ dmit2015-firebase-app/src/main/java/dmit2015/restclient/VideoGame.java
@@ -1,10 +1,18 @@
 package dmit2015.restclient;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class VideoGame {
 
+    @NotBlank(message = "Platform cannot be blank")
     private String platform;
+    @NotBlank(message = "Title cannot be blank")
+    @Size(min=3, max = 20, message = "Title must contain 3 to 20 characters.")
     private String title;
 
+    @NotEmpty(message = "Genre cannot be empty")
     private String genre;
 
     public String getPlatform() {
